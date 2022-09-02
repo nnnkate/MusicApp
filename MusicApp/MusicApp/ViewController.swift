57b000d05c1b8d4d0277a123a7b893001d8c7835
Kate Nedelko <nedelkoev@mail.ru>
2022-09-02 21:33:26 +0300

    Create project, delete storyboard

---
+++ MusicApp/MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Ekaterina Nedelko on 2.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
